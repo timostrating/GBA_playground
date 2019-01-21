9d840ad9a1e503d4c298065ae3795b3354f04e59
timostrating <timostrating@home.nl>
2019-01-21 01:36:57 +0100

    second

--- app_first/first.c
+++ app_first/first.c
@@ -6,7 +6,7 @@ int main()
 {
     *(unsigned int*)0x04000000 = 0x0403;
 
-    ((unsigned short*)0x06000000)[120+80*240] = 0x001F;
+    ((unsigned short*)0x06000000)[120+80*240] = 0xF0F0;
     ((unsigned short*)0x06000000)[136+80*240] = 0x03E0;
     ((unsigned short*)0x06000000)[120+96*240] = 0x7C00;
 
